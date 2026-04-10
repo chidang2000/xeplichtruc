3d7e60df3855e608e75568b444d50adbd49b21d9
chidang2000 <nguyenchidang2k@gmail.com>
2026-04-10 09:30:05 +0700

    push n

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/health', (req, res) => res.json({ status: 'ok' }))
+
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/shifts', shiftRoutes)
